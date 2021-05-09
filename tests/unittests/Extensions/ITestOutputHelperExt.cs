1c2d7079d63454f21c1fd886aaeec61004c7eea9
Morten Brudvik <morten@brudvik.com>
2021-05-09 20:32:27 +0200

    fixed an minor issue

---
+++ tests/unittests/Extensions/ITestOutputHelperExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LanguageExt;
+using OfficeInteropLib.Common;
+using OfficeInteropLib.Extensions;
+using Xunit.Abstractions;
+
+namespace UnitTests.Extensions
+{
+    public static class ITestOutputHelperExt
+    {
+        public static Unit TimeInSeconds<T>(ITestOutputHelper logger, Action operation) =>
+            TimeInSeconds(logger, operation.ToFunc());
+
+        public static T TimeInSeconds<T>(ITestOutputHelper logger, Func<T> operation) =>
+            Measure.TimeInSeconds(operation).Map((timeInSeconds, result) =>
+            {
+                logger.WriteLine($"Execution time: {timeInSeconds} seconds");
+                return result;
+            });
+    }
+}
